a3f3694c1ef0cb4729c86165d583031c9a3e36b6
Juandi004 <jdgutierrez.2@sudamericano.edu.ec>
2024-01-22 12:32:49 -0500

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Iterables
+
+let word="Hoy es Lunes";
+
+for (let char of word){
+    alert(char);
+}
+
+/*este método permite iterar un string o Array, de forma que salga un alert
+hasta que se termine la palbra (en el caso de string), y en Arrays, hasta que
+salga un alert para cada elemento*/
